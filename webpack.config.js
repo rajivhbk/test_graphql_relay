a5ee255509ac35b13f2b45f9cfab5a079e173719
Rajiv Choudhary <rajiv.choudhary@zinier.com>
2018-10-08 21:15:19 +0530

    added babel plugin for class properties

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,32 @@
-const path = require('path');
+const path = require("path");
 
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
-  filename: "./index.html"
+    template: "./src/index.html",
+    filename: "./index.html"
 });
 
 module.exports = {
-  entry: ['@babel/polyfill', './src/index.js'],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env','@babel/react']
-          }
-        }
-      }
-    ]
-  },
-  plugins: [htmlPlugin]
+    entry: ["@babel/polyfill", "./src/index.js"],
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env","@babel/react"],
+                        plugins: ["@babel/plugin-proposal-class-properties"]
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [htmlPlugin]
 };
